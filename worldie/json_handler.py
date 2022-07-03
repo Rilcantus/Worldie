237bd0e988792095d430b96c158a0d07f0bbb9ad
Rilcantus <rileyracer62@yahoo.com>
2022-07-02 21:22:22 -0700

    created working json appender! needs refactor..

--- worldie/json_handler.py
+++ worldie/json_handler.py
@@ -7,16 +7,48 @@ class JsonHand:
     def __init__(self, title, info):
         self.title = title
         self.info = info
+        self.converted = {}
+        self.current_json = {}
+        self.compared = ['']
 
-
+        self._pull()
         self._convert()
+        self._compare()
+        self._push()
 
+        print(self.compared)
 
+    def _pull(self):
+        
+        j_file = open('worldie\data.json')
+        self.current_json = json.load(j_file)
+    
     def _convert(self):
-        data = {self.title : self.info}
+        self.converted = {self.title: self.info}
 
-        with open('data.json', 'w') as outfile:
-            json.dump(data, outfile)
+    def _compare(self):
+        for type, elements in self.current_json.items():
+            for new_type, new_elements in self.converted.items():
+                if type == new_type:
+                    for item in new_elements:
+                        if item not in elements:
+                            self.compared = item
+
+    def _push(self):
+        for work, elements in self.current_json.items():
+            if type(self.compared) == str:
+                elements.append(self.compared)
+            elif type(self.compared) == list:
+                for item in self.compared:
+                    elements.append(item)
+        
+        with open('worldie\data.json', 'w') as outfile:
+            json.dump(self.current_json, outfile)
+
+       
+        
+
+
         
 
         
