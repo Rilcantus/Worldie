56f45933f3060877959e5922998fc7c330ac6034
Rilcantus <rileyracer62@yahoo.com>
2022-07-04 13:48:10 -0700

    working json pushing, overwrite data, need to append need work

--- worldie/json_handler.py
+++ worldie/json_handler.py
@@ -7,28 +7,47 @@ class JsonHand:
     def __init__(self, title, info):
         self.title = title
         self.info = info
-        self.outfile = r"worldie\data.json"
+        self.outfile = "worldie\data.json"
         self.work_types = {}
         self.current_json = {}
+        self.converted = {}
+        self.compared = {}
 
         self._pull()
+        self._covert()
+        self._compare()
+        self._push()
     
     def _pull(self):
         # pull data from .json file
-        self.current_json = json.load("worldie\data.json")
-        print(self.current_json)
+        try:
+            self.current_json = json.load(self.outfile)
+            print(self.current_json.items())
+        
+        except:
+            print("no data")
 
     def _covert(self):
         # converts in coming title and info to json ready
-        pass
+        self.converted = {self.title: self.info}
+        print(self.converted)
 
     def _compare(self):
         # check stored list to the incoming list and add only new info(keys)
-        pass
+        if self.title in self.current_json:
+            print("yes")
+        elif self.title not in self.current_json:
+            print("no {}".format(self.title))
+            self.compared = self.converted 
+            print(self.compared)
 
     def _push(self):
         # add new keys from compared to stored list then push back
-        pass
+        self.current_json[self.title = ]
+        with open (self.outfile, 'w') as outfile:
+            json.dump(self.current_json, outfile, indent=4)
+
+        
 
        
         
